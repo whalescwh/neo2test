cb5954f604dfc7fd8408ec5519e065ec813c3550
whalescwh <whalescwh@yahoo.com>
2018-05-17 07:59:04 +0800

    Create test5.php

---
+++ test5.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<form method=post>
+<table>
+<tr><td>User id:</td><td><input type=text name=fuserid></td></tr>
+<tr><td>Trans No Start:</td><td><input type=text name=fstart></td></tr>
+<tr><td>Trans No End:</td><td><input type=text name=fend></td></tr>
+<tr><td><input type=submit name=submit value="Create"></td><td><input type=submit name=submit value="Cancel"></td></tr>
+</table>
+</form>
+</body>
+</html>
