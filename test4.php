c683195140260318858855d5c6f311d6b7a4f734
whalescwh <whalescwh@yahoo.com>
2018-05-17 00:09:08 +0800

    Update test4.php

--- test4.php
+++ test4.php
@@ -1,5 +1,7 @@
 <?php
 $con = dbopen();
+$strsql = "select count(*) from trans inner join users on users.user_id = trans.user_id limit 5000";
+sql2table($strsql);
 $strsql = "select trans.trans_id, trans.trans_no, trans.user_id, login, name, trans.remarks from trans inner join users on users.user_id = trans.user_id limit 5000";
 sql2table($strsql);
 //$result = $con->query($strsql);
