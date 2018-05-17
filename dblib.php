d453965bee141f0de2c911348aa3738866e701ba
whalescwh <whalescwh@yahoo.com>
2018-05-17 20:14:00 +0800

    Update dblib.php

--- dblib.php
+++ dblib.php
@@ -20,18 +20,21 @@ function dbclose($con) {
 }
 //return sql as array
 function sql2a ($sqlstr) {
+	global $con;
 	$result = mysql_query($sqlstr);
 //?echo $sqlstr;
 	return $result;
 }
 //return sql as array
 function sql2json ($sqlstr) {
+	global $con;
 	$result = mysql_query($sqlstr);
 //?echo $sqlstr;
 	return json_encode( $result );
 }
 //sql update
 function sqlupdate ($sqlstr) {
+	global $con;
 	//echo $sqlstr;
 	$result = mysql_query($sqlstr);
 	return $result;
@@ -65,6 +68,7 @@ function sql2table ($sqlstr) {
 };
 //return sql as html table with last row as sum
 function sql2tablesum ($sqlstr, $sqlsum) {
+	global $con;
 	//echo $sqlstr;
 	$result = mysql_query($sqlstr);
 	echo "<table class=list><tr>";
@@ -103,6 +107,7 @@ function sql2tablesum ($sqlstr, $sqlsum) {
 };
 //return sql as form. assume return one row only
 function sql2form ($sqlstr) {
+	global $con;
 	$result = mysql_query($sqlstr);
 	echo "<table  class=form>";
 	$row = mysql_fetch_array($result);
@@ -118,6 +123,7 @@ function sql2form ($sqlstr) {
 };
 //return sql as edit form. assume return one row only
 function sql2edit ($sqlstr) {
+	global $con;
 	$result = mysql_query($sqlstr);
 	echo "<table border='1'>";
 	$row = mysql_fetch_array($result);
@@ -133,11 +139,13 @@ function sql2edit ($sqlstr) {
 };
 //return single value of a sql. e.g. echo sql2val('select count(*) from vuseraccess where uid=1 ')
 function sql2val($sqlstr) {
+	global $con;
 	$result = mysql_query($sqlstr);
 	if($row = mysql_fetch_array($result)) { return $row[0]; } else { return "";};
 }
 //return sql as select option list. e.g. echo "<select name=actid>" .sql2opt("select actid,actid from activity", "Fax")."</select>"
 function sql2opt($sqlstr, $default) {
+	global $con;
 //	$str="<option value='ALL'>ALL</option>";
 	$result = mysql_query($sqlstr);
 	while($row = mysql_fetch_array($result)) {
