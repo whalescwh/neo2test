3b0c827d971a57369300ae28758f9af1108314cf
whalescwh <whalescwh@yahoo.com>
2018-05-17 08:02:53 +0800

    Update test5.php

--- test5.php
+++ test5.php
@@ -2,7 +2,7 @@
 <body>
 <form method=post>
 <table>
-<tr><td>User id:</td><td><input type=text name=fuserid></td></tr>
+<tr><td>User id:</td><td><select name=fuserid><option value="whales">whales</option><option value="whales1">whales1</option></select></td></tr>
 <tr><td>Trans No Start:</td><td><input type=text name=fstart></td></tr>
 <tr><td>Trans No End:</td><td><input type=text name=fend></td></tr>
 <tr><td><input type=submit name=submit value="Create"></td><td><input type=submit name=submit value="Cancel"></td></tr>
