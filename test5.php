2d9dc2c0ba4b8738f2ecbfb08a07516891e3a36a
whalescwh <whalescwh@yahoo.com>
2018-05-17 08:15:58 +0800

    Update test5.php

--- test5.php
+++ test5.php
@@ -26,7 +26,7 @@ function showinput() {
 <tr><td>User id:</td><td><select name=fuserid><option value="whales">whales</option><option value="whales1">whales1</option></select></td></tr>
 <tr><td>Trans No Start:</td><td><input type=text name=fstart></td></tr>
 <tr><td>Trans No End:</td><td><input type=text name=fend></td></tr>
-<tr><td><input type=submit name=fsubmit value="Create"></td><td><input type=submit name=fsubmit value="Cancel"></td></tr>
+<tr><td><input type=submit name=fsubmit value="Create"></td><td><input type=submit name=fcancel value="Cancel"></td></tr>
 </table>
 </form>
 </body>
