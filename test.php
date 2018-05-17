c47744ae9d2b6c5510e09ecc0f951e8a7d152dd4
whalescwh <whalescwh@yahoo.com>
2018-05-18 00:55:02 +0800

    -

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
 echo "hello"
-date_default_timezone_set('UTC');
+//date_default_timezone_set('UTC');
 
 // Prints something like: Monday 8th of August 2005 03:12:46 PM
 echo date('l jS \of F Y h:i:s A');
 ?>
+-
+
