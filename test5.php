325d2aac642549155e894aed25a0689703f86fbc
whalescwh <whalescwh@yahoo.com>
2018-05-17 20:19:39 +0800

    Update test5.php

--- test5.php
+++ test5.php
@@ -1,25 +1,24 @@
 <?php
 include 'dblib.php';
 echo "<html><body>";
-    echo $_REQUEST["fuserid"];
-    echo $_REQUEST["fstart"];
-    echo $_REQUEST["fend"];
+   // echo $_REQUEST["fuserid"];
+    //echo $_REQUEST["fstart"];
+    //echo $_REQUEST["fend"];
 
     if ($_REQUEST["fstart"] <= 0) echo "<p>Start must be a number greater than zero";
     if ($_REQUEST["fend"] <= 0) echo "<p>End must be a number greater than zero";
     if ($_REQUEST["fstart"] < $_EQUEST["fend"]) echo "<p>End must not be samller than Start";
-$cnt = $_REQUEST["fstart"];
+    $cnt = $_REQUEST["fstart"];
     if ($cnt > 0 ) {
 	    $con = dbopen();
 	    
 		for ($cnt=$_REQUEST["fstart"]; $cnt <= $_REQUEST["fend"]; $cnt ++) {
 			$strsql = "insert into trans (trans_no, user_id, remarks) values (" . $cnt . ", ". $_REQUEST["fuserid"].", 'remarks ".$_REQUEST["fstart"]." to ".$_REQUEST["fend"]."')";
-			echo $strsql;
+			//echo $strsql;
 			sqlupdate($strsql);
 		};
+	    echo "Updated ". $cnt . " rows<br>";
 	    dbclose($con);
-	}	else {
-		echo "-";
 	}
 
 showinput();
