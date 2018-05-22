9787f4e9607d9296e848ed2db1ce4db4a14d4a33
whalescwh <whalescwh@yahoo.com>
2018-05-22 23:59:08 +0800

    aa

--- test6.php
+++ test6.php
@@ -16,11 +16,13 @@ echo "</body></html>";
 exit();
 
 function showinput() {
+	$strsql = $_REQUEST["fsql"];
+	if ($strsql == "") {$strsql = "select * from users inner join trans on trans.user_id = users.user_id"; };
   ?>
 
 <form method=post>
 <table>
-<tr><td><textarea name=fsql rows=6 cols=50></textarea></td></tr>
+<tr><td><textarea name=fsql rows=6 cols=80><? echo $_REQUEST["fsql"]; ?></textarea></td></tr>
 <tr><td><input type=submit name=fsubmit value="Run SQL"><input type=reset value="Cancel"></td></tr>
 </table>
 </form>
