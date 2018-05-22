ad5149079bd14869e6adc8ebccd1206d47ddaf1e
whalescwh <whalescwh@yahoo.com>
2018-05-22 23:31:08 +0800

    kk

--- test4.php
+++ test4.php
@@ -3,7 +3,7 @@ include 'dblib.php';
 $con = dbopen();
 $strsql = "select count(*) from (select trans.trans_id from trans inner join users on users.user_id = trans.user_id limit 5000) aa ";
 sql2table($strsql);
-$strsql = "select trans.trans_id, trans.trans_no, trans.user_id, login, name, trans.remarks from trans inner join users on users.user_id = trans.user_id limit 5000";
+$strsql = "select trans.trans_id, trans.trans_no, trans.user_id, login,  trans.remarks from trans inner join users on users.user_id = trans.user_id limit 5000";
 sql2table($strsql);
 //$result = $con->query($strsql);
 //echo $result->field_count;
