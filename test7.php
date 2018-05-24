620d3f9a7f92a07ffd5006bfe75446013f29426d
whalescwh <whalescwh@yahoo.com>
2018-05-24 09:05:15 +0800

    test7

--- test7.php
+++ test7.php
@@ -1,6 +1,15 @@
 <?php
+include 'dblib.php';
+$con = dbopen();
+$strsql = "select format(count(*),0) from trans";
+//$result = $con->query($strsql);
+//echo $result->field_count;
+
 		echo date("H:m:s") . " - 1<br>";		
+sql2table($strsql);
 				echo date("H:m:s") . " - 2<br>";
+sql2table($strsql);
 				echo date("H:m:s") . " - 3<br>";
+dbclose($con);
 				?>
 				
